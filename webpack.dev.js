69f7cffed43d9b4eda601c029cc322e3546ca885
Vlad Tymoshchyk <vtymoshchyk@gmail.com>
2023-09-20 23:43:52 +0300

    feat: setup typescript

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,5 +2,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.ts?/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
   plugins: [new HtmlWebpackPlugin()],
 };
